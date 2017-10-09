a4cff17e800a20ee9f017e93c6acd50330994152
wencakisa <wencakisa@gmail.com>
2017-10-09 17:50:00 +0300

    Implemented linear regression task

--- app/controllers/lin_regressions_controller.rb
+++ app/controllers/lin_regressions_controller.rb
@@ -1,2 +1,22 @@
+require 'csv'
+require 'linear-regression/linear'
+
 class LinRegressionsController < ApplicationController
+  def create
+    file = params[:file]
+
+    lines = CSV.read file.path
+
+    y_values = lines.collect(&:first).map(&:to_f)
+    x_values = (1..y_values.length).to_a
+
+    linear_regression = Regression::Linear.new x_values, y_values
+    slope = linear_regression.slope
+    intercept = linear_regression.intercept
+
+    slope_formatted = '%.6f' % slope
+    intercept_formatted = '%.6f' % intercept
+
+    render plain: "#{slope_formatted},#{intercept_formatted}"
+  end
 end
