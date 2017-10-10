c90738c12792daf4667c576d824fd1bb79457aef
wencakisa <wencakisa@gmail.com>
2017-10-10 16:29:21 +0300

    Used map over collect

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -4,7 +4,7 @@ class IntervalsController < ApplicationController
   def create
     sums = Array.new
     CSV.read(params[:file].path).each_cons(30) do |chunk|
-      sums.push chunk.collect(&:first).map(&:to_f).reduce(&:+)
+      sums.push chunk.map(&:first).map(&:to_f).reduce(&:+)
     end
 
     render plain: '%.2f' % sums.max
--- app/controllers/sums_controller.rb
+++ app/controllers/sums_controller.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 class SumsController < ApplicationController
   def create
-    result = CSV.read(params[:file].path).collect(&:first).map(&:to_f).reduce(&:+)
+    result = CSV.read(params[:file].path).map(&:first).map(&:to_f).reduce(&:+)
 
     render plain: '%.2f' % result
   end
