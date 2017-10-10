afdffd72e569bf2498f440466d3f8c8c319a63d1
wencakisa <wencakisa@gmail.com>
2017-10-10 16:29:34 +0300

    Used size over length

--- app/controllers/lin_regressions_controller.rb
+++ app/controllers/lin_regressions_controller.rb
@@ -3,8 +3,8 @@ require 'linear-regression/linear'
 
 class LinRegressionsController < ApplicationController
   def create
-    y_values = CSV.read(params[:file].path).collect(&:first).map(&:to_f)
-    x_values = (1..y_values.length).to_a
+    y_values = CSV.read(params[:file].path).map(&:first).map(&:to_f)
+    x_values = (1..y_values.size).to_a
     reg = Regression::Linear.new x_values, y_values
 
     render plain: '%.6f,%.6f' % [reg.slope, reg.intercept]
