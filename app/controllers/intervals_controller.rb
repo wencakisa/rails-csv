466304cadb5fd944c8ba3e80beb22e886c65b638
wencakisa <wencakisa@gmail.com>
2017-10-22 12:26:37 +0300

    Minor changes

--- app/controllers/intervals_controller.rb
+++ app/controllers/intervals_controller.rb
@@ -2,9 +2,9 @@ require 'csv'
 
 class IntervalsController < ApplicationController
   def create
-    sums = Array.new
+    sums = []
     CSV.foreach(params[:file].path).each_cons(30) do |chunk|
-      sums.push chunk.map(&:first).map(&:to_f).reduce(&:+)
+      sums << chunk.map(&:first).map(&:to_f).reduce(&:+)
     end
 
     render plain: '%.2f' % sums.max
--- app/controllers/lin_regressions_controller.rb
+++ app/controllers/lin_regressions_controller.rb
@@ -5,8 +5,8 @@ class LinRegressionsController < ApplicationController
   def create
     x_values, y_values = [], []
     CSV.foreach(params[:file].path).with_index(1) do |row, index|
-      x_values.push index
-      y_values.push row.first.to_f
+      x_values << index
+      y_values << row.first.to_f
     end
 
     reg = Regression::Linear.new x_values, y_values
